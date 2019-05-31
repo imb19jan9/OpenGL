9bad69dc38f902dcb3e72307b5f554bc746b4ffc
Seongje Jang <31645640+imb19jan9@users.noreply.github.com>
2019-05-31 12:09:18 +0900

    Basic VBO Triangle rendering

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -2,20 +2,40 @@
 #include <QOpenGLWidget>
 #include <QSurfaceFormat>
 #include <iostream>
+#include <QOpenGLFunctions>
+#include <QOpenGLContext>
 
 class OpenGL : public QOpenGLWidget {
+private:
+	float positions[6] = {
+		-0.5f, -0.5f,
+		 0.5f, -0.5f,
+		 0.0f,  0.5f,
+	};
+
+	unsigned int vbo;
+
+private:
 	void initializeGL() {
-		std::cout << glGetString(GL_VERSION) << std::endl;
+		QOpenGLFunctions *f = QOpenGLContext::currentContext()->functions();
+
+		std::cout << f->glGetString(GL_VERSION) << std::endl;
+
+		f->glGenBuffers(1, &vbo);
+		f->glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		f->glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+		
+		f->glEnableVertexAttribArray(0);
+		f->glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
 	}
 
 	void paintGL() {
-		glClear(GL_COLOR_BUFFER_BIT);
+		QOpenGLFunctions *f = QOpenGLContext::currentContext()->functions();
 
-		glBegin(GL_TRIANGLES);
-		glVertex2f(-0.5f, -0.5f);
-		glVertex2f(0.5f, -0.5f);
-		glVertex2f(0.0f, 0.5f);
-		glEnd();
+		f->glClear(GL_COLOR_BUFFER_BIT);
+
+		f->glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		f->glDrawArrays(GL_TRIANGLES, 0, 3);
 	}
 };
 
