957863a845c0ab0498872bc39355c879b3be5ef8
Seongje Jang <31645640+imb19jan9@users.noreply.github.com>
2019-05-31 12:29:58 +0900

    Basic shader example

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -5,6 +5,55 @@
 #include <QOpenGLFunctions>
 #include <QOpenGLContext>
 
+unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+	QOpenGLFunctions *f = QOpenGLContext::currentContext()->functions();
+
+	unsigned int id = f->glCreateShader(type);
+	const char* src = source.c_str();
+	f->glShaderSource(id, 1, &src, 0);
+	f->glCompileShader(id);
+
+	int result;
+	f->glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+	if (result == GL_FALSE) {
+		int length;
+		f->glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+
+		char *message = new char[length * sizeof(char)];
+		f->glGetShaderInfoLog(id, length, &length, message);
+
+		std::cout << "Failed to compile " <<
+			(type == GL_VERTEX_SHADER ? "vertex " : "fragment ") <<
+			"shader!" << std::endl;
+		std::cout << message << std::endl;
+
+		delete message;
+
+		return 0;
+	}
+
+	return id;
+}
+
+unsigned int CreateShaderProgram(const std::string& vertexShader, const std::string& fragmentShader) {
+	QOpenGLFunctions *f = QOpenGLContext::currentContext()->functions();
+
+	unsigned int program = f->glCreateProgram();
+	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+	f->glAttachShader(program, vs);
+	f->glAttachShader(program, fs);
+	f->glLinkProgram(program);
+	f->glValidateProgram(program);
+
+	f->glDeleteShader(vs);
+	f->glDeleteShader(fs);
+
+	return program;
+}
+
 class OpenGL : public QOpenGLWidget {
 private:
 	float positions[6] = {
@@ -14,12 +63,43 @@ private:
 	};
 
 	unsigned int vbo;
+	unsigned int program;
+	bool initialized;
+	
+public:
+	OpenGL() : initialized(false) {	}
+	~OpenGL() {
+		if (initialized) {
+			QOpenGLFunctions *f = QOpenGLContext::currentContext()->functions();
+			f->glDeleteBuffers(1, &vbo);
+			f->glDeleteProgram(program);
+		}
+	}
 
 private:
 	void initializeGL() {
 		QOpenGLFunctions *f = QOpenGLContext::currentContext()->functions();
 
 		std::cout << f->glGetString(GL_VERSION) << std::endl;
+
+		std::string vertexShader =
+			"#version 330 core\n"
+			"\n"
+			"layout(location = 0) in vec4 position;\n"
+			"\n"
+			"void main() {\n"
+			"  gl_Position = position;\n"
+			"}\n";
+		std::string fragmentShader =
+			"#version 330 core\n"
+			"\n"
+			"out vec4 color;\n"
+			"\n"
+			"void main() {\n"
+			"  color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+			"}\n";
+
+		program = CreateShaderProgram(vertexShader, fragmentShader);
 
 		f->glGenBuffers(1, &vbo);
 		f->glBindBuffer(GL_ARRAY_BUFFER, vbo);
@@ -34,6 +114,7 @@ private:
 
 		f->glClear(GL_COLOR_BUFFER_BIT);
 
+		f->glUseProgram(program);
 		f->glBindBuffer(GL_ARRAY_BUFFER, vbo);
 		f->glDrawArrays(GL_TRIANGLES, 0, 3);
 	}
