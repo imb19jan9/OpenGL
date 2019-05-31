34ed6dc3bd872ca884e78c6c353a9bbcff10ec89
Seongje Jang <31645640+imb19jan9@users.noreply.github.com>
2019-05-31 16:09:54 +0900

    VAO Rendering

--- OpenGL/src/main.cpp
+++ OpenGL/src/main.cpp
@@ -1,7 +1,7 @@
 #include <QtWidgets/QApplication>
 #include <QOpenGLWidget>
 #include <QSurfaceFormat>
-#include <QOpenGLFunctions>
+#include <QOpenGLFunctions_3_3_Core>
 #include <QOpenGLContext>
 
 #include <string>
@@ -14,11 +14,13 @@
 	ASSERT(GLLogCall(#x, __FILE__, __LINE__))
 
 void GLClearError() {
-	while (glGetError() != GL_NO_ERROR);
+	QOpenGLFunctions_3_3_Core *f = QOpenGLContext::currentContext()->versionFunctions<QOpenGLFunctions_3_3_Core>();
+	while (f->glGetError() != GL_NO_ERROR);
 }
 
 bool GLLogCall(const char* function, const char* file, int line) {
-	if (GLenum error = glGetError()) {
+	QOpenGLFunctions_3_3_Core *f = QOpenGLContext::currentContext()->versionFunctions<QOpenGLFunctions_3_3_Core>();
+	if (GLenum error = f->glGetError()) {
 		std::cout << "[OpenGL Error] (" << error << ") : " << function << ", "
 			<< file << " : " << line << std::endl;
 		return false;
@@ -39,7 +41,7 @@ std::string LoadShader(const std::string& filepath) {
 }
 
 unsigned int CompileShader(unsigned int type, const std::string& source) {
-	QOpenGLFunctions *f = QOpenGLContext::currentContext()->functions();
+	QOpenGLFunctions_3_3_Core *f = QOpenGLContext::currentContext()->versionFunctions<QOpenGLFunctions_3_3_Core>();
 
 	GLCall(unsigned int id = f->glCreateShader(type));
 	const char* src = source.c_str();
@@ -69,7 +71,7 @@ unsigned int CompileShader(unsigned int type, const std::string& source) {
 }
 
 unsigned int CreateShaderProgram(const std::string& vertexShader, const std::string& fragmentShader) {
-	QOpenGLFunctions *f = QOpenGLContext::currentContext()->functions();
+	QOpenGLFunctions_3_3_Core *f = QOpenGLContext::currentContext()->versionFunctions<QOpenGLFunctions_3_3_Core>();
 
 	GLCall(unsigned int program = f->glCreateProgram());
 	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
@@ -100,17 +102,21 @@ private:
 		2, 3, 0
 	};
 
+	unsigned int vao;
 	unsigned int vbo;
 	unsigned int ibo;
 	unsigned int program;
 	int location;
+
 	bool initialized;
 	
 public:
 	OpenGL() : initialized(false) {	}
 	~OpenGL() {
 		if (initialized) {
-			QOpenGLFunctions *f = QOpenGLContext::currentContext()->functions();
+			QOpenGLFunctions_3_3_Core *f = QOpenGLContext::currentContext()->versionFunctions<QOpenGLFunctions_3_3_Core>();
+			
+			GLCall(f->glDeleteVertexArrays(1, &vao));
 			GLCall(f->glDeleteBuffers(1, &vbo));
 			GLCall(f->glDeleteBuffers(1, &ibo));
 			GLCall(f->glDeleteProgram(program));
@@ -119,7 +125,7 @@ public:
 
 private:
 	void initializeGL() {
-		QOpenGLFunctions *f = QOpenGLContext::currentContext()->functions();
+		QOpenGLFunctions_3_3_Core *f = QOpenGLContext::currentContext()->versionFunctions<QOpenGLFunctions_3_3_Core>();
 
 		GLCall(std::cout << f->glGetString(GL_VERSION) << std::endl);
 
@@ -131,8 +137,10 @@ private:
 		GLCall(f->glUseProgram(program));
 		GLCall(location = f->glGetUniformLocation(program, "u_Color"));
 		ASSERT(location != -1);
-		GLCall(f->glUniform4f(location, 0.8f, 0.3f, 0.8f, 1.0f));
 
+		GLCall(f->glGenVertexArrays(1, &vao));
+		GLCall(f->glBindVertexArray(vao));
+		
 		GLCall(f->glGenBuffers(1, &vbo));
 		GLCall(f->glBindBuffer(GL_ARRAY_BUFFER, vbo));
 		GLCall(f->glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(float), positions, GL_STATIC_DRAW));
@@ -146,7 +154,7 @@ private:
 	}
 
 	void paintGL() {
-		QOpenGLFunctions *f = QOpenGLContext::currentContext()->functions();
+		QOpenGLFunctions_3_3_Core *f = QOpenGLContext::currentContext()->versionFunctions<QOpenGLFunctions_3_3_Core>();
 
 		GLCall(f->glClear(GL_COLOR_BUFFER_BIT));
 
@@ -167,7 +175,7 @@ private:
 		GLCall(f->glUseProgram(program));
 		GLCall(f->glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
 
-		GLCall(f->glBindBuffer(GL_ARRAY_BUFFER, vbo));
+		GLCall(f->glBindVertexArray(vao));
 		GLCall(f->glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
 		GLCall(f->glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0));
 	}
@@ -176,7 +184,12 @@ private:
 int main(int argc, char *argv[]) {
 	QApplication a(argc, argv);
 
+	QSurfaceFormat format;
+	format.setVersion(3, 3);
+	format.setProfile(QSurfaceFormat::CoreProfile);
+
 	OpenGL openGL;
+	openGL.setFormat(format);
 	openGL.show();
 
 	return a.exec();
