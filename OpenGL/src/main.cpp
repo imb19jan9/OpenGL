b1145a7a41665a3093ac4120e506dd8b71aca676
Seongje Jang <31645640+imb19jan9@users.noreply.github.com>
2019-06-02 16:17:04 +0900

    Added QtGUI

--- OpenGL/src/main.cpp
+++ OpenGL/src/main.cpp
@@ -3,8 +3,12 @@
 #include <QSurfaceFormat>
 #include <QOpenGLFunctions_3_3_Core>
 #include <QOpenGLContext>
-#include <iostream>
 #include <QMatrix4x4>
+#include <QSlider>
+#include <QBoxLayout>
+#include <QTimer>
+#include <QObject>
+#include <iostream>
 
 #include "VAO.h"
 #include "VBO.h"
@@ -13,6 +17,31 @@
 #include "VBOLayout.h"
 #include "Renderer.h"
 #include "Texture.h"
+
+class Sliders : public QWidget {
+private:
+	QBoxLayout layout;
+	QSlider vSlider, hSlider;
+	
+public:
+	Sliders(QWidget* parent = 0, Qt::WindowFlags f = Qt::WindowFlags())
+		: QWidget(parent, f),
+		layout(QBoxLayout::LeftToRight, this),
+		vSlider(Qt::Vertical, this), hSlider(Qt::Horizontal, this) {
+		resize(200, 200);
+		layout.addWidget(&vSlider);
+		layout.addWidget(&hSlider);
+		setLayout(&layout);
+
+		vSlider.setMinimum(0);
+		vSlider.setMaximum(100);
+		hSlider.setMinimum(0);
+		hSlider.setMaximum(100);
+	}
+
+	inline int GetVSliderValue() { return vSlider.value(); }
+	inline int GetHSliderValue() { return hSlider.value(); }
+};
 
 class OpenGL : public QOpenGLWidget {
 private:
@@ -34,9 +63,17 @@ private:
 	ShaderProgram *program;
 	Renderer renderer;
 	Texture *texture;
+
+	Sliders *sliders;
+
+	QTimer *timer;
 	
 public:
-	OpenGL() : vao(nullptr), vbo(nullptr), ibo(nullptr), program(nullptr) {}
+	OpenGL() : vao(nullptr), vbo(nullptr), ibo(nullptr), program(nullptr) {
+		sliders = new Sliders(this, Qt::Window);
+		sliders->setVisible(true);
+
+	}
 	~OpenGL() {
 		makeCurrent();
 
@@ -45,6 +82,8 @@ public:
 		delete ibo;
 		delete program;
 		delete texture;
+
+		delete sliders;
 	}
 
 private:
@@ -56,13 +95,6 @@ private:
 		program = new ShaderProgram("res/shaders/Basic.vertex", "res/shaders/Basic.fragment");
 		program->Bind();
 		program->SetUniform1i("u_Texture", 0);
-		
-		QMatrix4x4 model, view, proj, mvp;
-		model.translate(2, 0, 0);
-		view.translate(-1.5, 0, 0);
-		proj.ortho(-2.0, 2.0, -1.5, 1.5, -1.0, 1.0);
-		mvp = proj * view * model;
-		program->SetUniformMat4f("u_MVP", mvp.data());
 
 		texture = new Texture("res/textures/hccl_logo.png");
 
@@ -85,8 +117,19 @@ private:
 
 		renderer.Clear();
 
+		QMatrix4x4 model, view, proj, mvp;
+		float posx = (sliders->GetHSliderValue() / 100.0 - 0.5) * 4;
+		float posy = (sliders->GetVSliderValue() / 100.0 - 0.5) * 3;
+		model.translate(posx, posy, 0);
+		view.translate(0, 0, 0);
+		proj.ortho(-2.0, 2.0, -1.5, 1.5, -1.0, 1.0);
+		mvp = proj * view * model;
+		program->SetUniformMat4f("u_MVP", mvp.data());
+
 		texture->Bind();
 		renderer.Draw(*vao, *ibo, *program);
+
+		update();
 	}
 };
 
@@ -96,10 +139,10 @@ int main(int argc, char *argv[]) {
 	QSurfaceFormat format;
 	format.setVersion(3, 3);
 	format.setProfile(QSurfaceFormat::CoreProfile);
+	QSurfaceFormat::setDefaultFormat(format);
 
 	OpenGL openGL;
 	openGL.resize(800, 600);
-	openGL.setFormat(format);
 	openGL.show();
 
 	return a.exec();
